47337a0f32225c493b5f0e5fc1218d0f1b892a1f
daqingzhang <daqingzhang@rdamicro.com>
2017-11-02 18:56:22 +0800

    update c1

--- c1/main.c
+++ c1/main.c
@@ -4,16 +4,17 @@
 int main(int argc, const char *argv[])
 {
 	int n, a = 1, b = 1;
+	const char *p;
 
 	printf("argc=%d\n", argc);
-	if(argc > 0) {
+	if (argc > 0) {
 		const char *fname = argv[0];
 		printf("file name: %s\n", fname);
 	}
-	if (argc > 1)
-		a = *(int *)(argv[1]);
-	if (argc > 2)
-		b = *(int *)(argv[2]);
+	if (argc > 1) {
+		p = argv[1];
+		printf("argv[1]: %s\n", p);
+	}
 
 	n = func_add2(a,b);
 	printf("n = %d\n", n);
