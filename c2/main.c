57d10232615008d8cf9f66118e9da162c5843069
daqingzhang <daqingzhang@rdamicro.com>
2017-11-03 16:13:59 +0800

    Update c2

--- c2/main.c
+++ c2/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <unistd.h>
 
 static const struct option long_options[] = {
 	{ "help",	0, NULL, 'h' },
@@ -21,7 +22,7 @@ static void print_usage(FILE *stream, int exit_code)
 	exit(exit_code);
 }
 
-int main(int argc, char *argv[])
+int test_opt(int argc, char *argv[])
 {
 	int next_option;
 	char *outfile;
@@ -43,10 +44,10 @@ int main(int argc, char *argv[])
 			outfile = optarg;
 			fprintf(stdout, "outfile: %s\n", outfile);
 			break;
-		case '?':
+		case '?': // invalid option
 			print_usage(stderr, 1);
 			break;
-		case -1:
+		case -1: // no more options are found
 			break;
 		default:
 			abort();//abort exception, generate core dump
@@ -61,15 +62,14 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-#if 0
-int main(int argc, const char *argv[])
+int loop_arg(int argc, char *argv[])
 {
 	int i;
-	const char *p;
+	char *p;
 
 	printf("argc=%d\n", argc);
 	if (argc > 0) {
-		const char *fname = argv[0];
+		char *fname = argv[0];
 
 		printf("file name: %s\n", fname);
 		for(i = 1;i < argc; i++) {
@@ -79,4 +79,92 @@ int main(int argc, const char *argv[])
 	}
 	return 0;
 }
+
+
+int test_ioe(int argc, char *argv[])
+{
+	printf("start\n");
+#if 0
+	while (1) {
+		printf(".\n");
+		sleep(1);
+	}
 #endif
+
+#if 1
+	while (1) {
+		fprintf(stdout, ".");
+		sleep(1);
+	}
+#endif
+
+#if 0
+	while (1) {
+		/*
+		 * stderr is not buffered, it will send data
+		 * to console directly
+		 */
+		fprintf(stderr, ".");
+		sleep(1);
+	}
+#endif
+
+#if 0
+	while (1) {
+		/*
+		 * stdout is buffered, it will send data to console
+		 * until buffered is filled('\n' is checked)
+		 */
+		printf(".");
+		fflush(stdout);
+		sleep(1);
+	}
+#endif
+
+	printf("end\n");
+	return 0;
+}
+
+int test_env(int argc, char *argv[])
+{
+	const char *penv, *pval;
+
+	if (argc > 0)
+		printf("prg name: %s\n", argv[0]);
+
+	if (argc > 1) {
+		penv = argv[1];
+		pval = getenv(penv);
+		printf("old env %s = %s\n", penv, pval);
+	} else {
+		penv = "ABCDEF";
+	}
+
+	if (argc > 2)
+		pval = argv[2];
+	else
+		pval = "goodcode";
+
+	printf("set env %s = %s\n", penv, pval);
+	setenv(penv, pval, 1);
+
+	pval = getenv(penv);
+	printf("get env %s = %s\n", penv, pval);
+	return 0;
+}
+
+
+int test_opt(int argc, char *argv[]);
+int loop_arg(int argc, char *argv[]);
+int test_ioe(int argc, char *argv[]);
+int test_env(int argc, char *argv[]);
+
+int main(int argc, char *argv[])
+{
+//	test_opt(argc, argv);
+//	loop_arg(argc, argv);
+//	test_ioe(argc, argv);
+	test_env(argc, argv);
+
+	return 0;
+}
