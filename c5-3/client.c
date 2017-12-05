8fee2c6169d9839fa3453cb0a498acbc0a03d6cc
daqingzhang <daqingzhang@rdamicro.com>
2017-12-05 17:31:15 +0800

    update c5-3: 1. A socket example for using localhost. The server implements a simple loopback for strings tx/rx. it can be connected by telnet via port 1234. The client just can connects the server only once. 2. add functions for showing host information.

--- c5-3/client.c
+++ c5-3/client.c
@@ -1,79 +1,100 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
 #include <errno.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
-int write_text(int socket_fd, char *text)
+static int send_message(int socket_fd, char *msg)
 {
-	int len = strlen(text);
-	int r;
+	char *end = "\r\n";
+	int len = strlen(msg), r;
 
-	r = write(socket_fd, &len, sizeof(len));
-
-	r += write(socket_fd, text, len);
-
+	r = write(socket_fd, msg, len);
+	r = write(socket_fd, end, 2);
 	return r;
 }
 
-#define BUF_SIZE 10000
-static char buffer[BUF_SIZE];
-int get_home_page(int socket_fd)
+int client_sock(int argc, char **argv)
 {
-	int len = 0;
-	ssize_t num;
+	int fd, r, i;
+	struct sockaddr_in sa;
+	int port = 1234;
+	char *ipstr = "127.0.0.1";
 
-	sprintf(buffer, "GET /\n");
-
-	len = write(socket_fd, buffer, strlen(buffer));
-
-	while (1) {
-		num = read(socket_fd, buffer, BUF_SIZE);
-		if (num == 0)
-			return 0;
-		printf("num = %d\n", (int)num);
-		fwrite(buffer, sizeof(char), num, stdout);
+	fd = socket(PF_INET, SOCK_STREAM, 0);
+	if (fd < 0) {
+		perror("socket failed\n");
+		return -1;
 	}
-	return len;
+	printf("socket fd=%d\n", fd);
+
+	sa.sin_family = AF_INET;
+	sa.sin_addr.s_addr = inet_addr(ipstr);
+	sa.sin_port = htons(port);
+
+	r = connect(fd, (const struct sockaddr *)&sa, sizeof(sa));
+	if (r) {
+		perror("connect failed\n");
+		return -1;
+	}
+	printf("connet server ok, ip: %s, port: %d\n", ipstr, port);
+
+	for(i = 1; i < argc; i++) {
+		send_message(fd, argv[i]);
+		sleep(1);
+	}
+	close(fd);
+	printf("client exit\n");
+	return 0;
+}
+
+int show_host_info(int argc, char **argv)
+{
+	char *host;
+	struct hostent *hinfo;
+	char myname[255];
+	char **alias, **addrs;
+
+	if (argc > 1) {
+		host = argv[1];
+	} else {
+		gethostname(myname, 255);
+		host = myname;
+	}
+	printf("host name: %s\n", host);
+
+	hinfo = gethostbyname(host);
+	if (!hinfo) {
+		printf("get host info failed\n");
+		return -1;
+	}
+	printf("hinfo, name: %s, addr type: %d, addr len: %d\n",
+		hinfo->h_name, hinfo->h_addrtype, hinfo->h_length);
+
+	alias = hinfo->h_aliases;
+	while(*alias) {
+		printf("hinfo, alias: %s\n", *alias);
+		alias++;
+	}
+
+	printf("AF_INET=%d, PF_INET=%d\n", AF_INET, PF_INET);
+	addrs = hinfo->h_addr_list;
+	while(*addrs) {
+		struct in_addr *pin = (struct in_addr *)*addrs;
+		printf("hinfo, addr: %s\n", inet_ntoa(*pin));
+		addrs++;
+	}
+	return 0;
 }
 
 int main(int argc, char *argv[])
 {
-	int socket_fd, r;
-	struct sockaddr_in name;
-	struct hostent *hostinfo;
-
-	if (argc < 2) {
-		printf("host name ?\n");
-		return -1;
-	}
-
-	socket_fd = socket(PF_INET, SOCK_STREAM, 0);
-
-	name.sin_family = AF_INET;
-
-	hostinfo = gethostbyname(argv[1]);
-	if(hostinfo == NULL)
-		return -1;
-	else
-		name.sin_addr = *((struct in_addr *)hostinfo->h_addr);
-
-	name.sin_port = htons(80);
-
-	r = connect(socket_fd, (const struct sockaddr *)&name,
-		sizeof(struct sockaddr_in));
-
-	if (r < 0) {
-		printf("connect failed\n");
-		return -1;
-	}
-	get_home_page(socket_fd);
-
-	close(socket_fd);
-	printf("disconnect server\n");
+	show_host_info(argc, argv);
+//	client_sock(argc, argv);
 	return 0;
 }
--- c5-3/server.c
+++ c5-3/server.c
@@ -6,87 +6,122 @@
 #include <unistd.h>
 #include <errno.h>
 
-int server(int client_socket)
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+static int server_run(int client_fd)
 {
-	int disconnect, len, r;
-	char *text;
+#define SERVER_BUF_SIZE 1024
+	int quit = 0;
+	int cnt, len, size;
+	char *ps;
+	char *end = "\r\n";
+	char *start = ">";
 
-	disconnect = 0;
-	while (1) {
-		r = read(client_socket, &len, sizeof(len));
-		if (!r)
-			break;
+	ps = (char *)malloc(SERVER_BUF_SIZE);
+	size = SERVER_BUF_SIZE;
 
-		text = (char *)malloc(len + 1);
+	do {
+		memset(ps, 0x0, size);
 
-		memset(text, 0, len + 1);
+		len = 0;
+		while (1) {
+			unsigned char t;
 
-		r = read(client_socket, text, len);
-		if (r)
-			printf("got message: %s\n", text);
+			cnt = read(client_fd, &t, 1);
+			if (cnt < 0) {
+				printf("read error, exit\n");
+				free(ps);
+				exit(-1);
+				break;
+			} else if (cnt > 0) {
+				printf("get char: %2x\n", t);
+				if (t == '\n') {
+					len--;
+					ps[len] = '\0';
+					break;
+				}
+				ps[len] = t;
+				len++;
+			} else {
+				usleep(20);
+			}
+		}
+		if (len) {
+			printf("msg:%s\n", ps);
+#if 1
+			cnt =write(client_fd, start, 2);
+			cnt += write(client_fd, ps, len);
+			cnt +=write(client_fd, end, 2);
+			printf("write %d bytes\n", cnt);
+#endif
+			if (!strcmp(ps, "quit"))
+				quit = 1;
+		//	break;
+		}
+	} while(!quit);
 
-		if (!strcmp(text, "quit"))
-			disconnect = 1;
-
-		free(text);
-
-		if (disconnect)
-			break;
-	}
-	return disconnect;
+	free(ps);
+	return quit;
 }
 
-int main(int argc, char *argv[])
+int server_sock(void)
 {
-	char *socket_name;
-	int socket_fd, quit, r;
-	struct sockaddr_un name;
+	int fd, quit, r;
+	struct sockaddr_in sa;
+	int port = 1234;
+	char *ipstr = "127.0.0.1";
 
-	if (argc < 2) {
-		printf("server 's name ?\n");
+	fd = socket(PF_INET, SOCK_STREAM, 0);
+	if (fd < 0) {
+		perror("get socket fd failed\n");
 		return -1;
 	}
-	socket_name = argv[1];
+	printf("fd=%d\n", fd);
 
-	printf("server %s is waitting connection ...\n", socket_name);
+	sa.sin_family = AF_INET;
+	sa.sin_port = htons(port);
+	sa.sin_addr.s_addr = inet_addr(ipstr);
 
-	socket_fd = socket(PF_LOCAL, SOCK_STREAM, 0);
-	if (socket_fd < 0) {
-		printf("create socket failed %d\n", errno);
-		return -1;
-	}
-	name.sun_family = AF_LOCAL;
-	strcpy(name.sun_path, socket_name);
-
-	r = bind(socket_fd, (const struct sockaddr *)&name, SUN_LEN(&name));
+	r = bind(fd, (const struct sockaddr *)&sa, sizeof(sa));
 	if (r) {
-		printf("bind socket failed %d\n", errno);
+		perror("bind socket failed\n");
 		return -1;
 	}
+	printf("bind ok, ip: %s, port: %d\n", ipstr, port);
 
-	r = listen(socket_fd, 5);
+	r = listen(fd, 5);
 	if (r) {
-		printf("listen socket failed %d\n", errno);
+		perror("listen socket failed\n");
 		return -1;
 	}
 
 	do {
-		struct sockaddr_un client_name;
-		socklen_t client_name_len;
-		int client_socket_fd;
+		int cli_fd;
+		struct sockaddr_in cli_sa;
+		socklen_t cli_len;
 
-		client_socket_fd = accept(socket_fd,
-			(struct sockaddr *)&client_name, &client_name_len);
+		printf("wait client connection ...\n");
+		cli_fd = accept(fd, (struct sockaddr *)&cli_sa, &cli_len);
+		if (cli_fd < 0) {
+			perror("client fd error\n");
+			continue;
+		}
+		printf("client fd=%d\n", cli_fd);
 
-		quit = server(client_socket_fd);
-
-		close(client_socket_fd);
+		quit = server_run(cli_fd);
+		printf("server exit\n");
+		close(cli_fd);
 	} while(!quit);
 
-	close(socket_fd);
-	unlink(socket_name);
+	close(fd);
+	printf("server closed\n");
 
-	printf("server %s disconnected\n", socket_name);
+	return 0;
+}
 
+int main(int argc, char *argv[])
+{
+	server_sock();
 	return 0;
 }
