eb45ebe502db82eb8c67dfe7bee65f64aa26fe41
daqingzhang <daqingzhang@rdamicro.com>
2017-12-14 14:47:59 +0800

    Update demo-env

--- demo-env/main.c
+++ demo-env/main.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 void print_indent(int depth, char prefix, char *name, char postfix)
 {
@@ -56,15 +57,92 @@ int scan_dir(char *name, int depth)
 	return 0;
 }
 
+int search_dir(const char *dir_name, const char *file_name, char *abs_path)
+{
+	int r = 0;
+	char *dname;
+	DIR *dir;
+	struct dirent *entry;
+	struct stat dstat;
+
+	dir = opendir(dir_name);
+	if (!dir) {
+		perror("opendir error:");
+		return -1;
+	}
+	chdir(dir_name);
+	while (1) {
+		entry = readdir(dir);
+		if (!entry) {
+			break;
+		}
+		dname = entry->d_name;
+		if ((!strcmp(dname, ".")) || (!strcmp(dname, ".."))) {
+			continue;
+		}
+		if (!strcmp(dname, file_name)) {
+			realpath(dname, abs_path);
+			printf("find %s: %s\n", file_name, abs_path);
+			r = 1;
+			break;
+		}
+		lstat(dname, &dstat);
+		if (S_ISDIR(dstat.st_mode)) {
+			r = search_dir(dname, file_name, abs_path);
+			if (r)
+				break;
+		}
+	}
+	chdir("..");
+	return r;
+}
+
+int get_path(void)
+{
+	char abs_path[256];
+
+	memset(abs_path, 0x0, sizeof(abs_path));
+	realpath(".", abs_path);
+	printf("abs_path: %s\n", abs_path);
+
+	chdir("..");
+	memset(abs_path, 0x0, sizeof(abs_path));
+	realpath(".", abs_path);
+	printf("abs_path: %s\n", abs_path);
+	return 0;
+}
+
 int main(int argc, char *argv[])
 {
-	char *path;
+	int r;
+	char *dir_name, *file_name;
+	char abs_path[256];
+
+	memset(abs_path, 0x0, sizeof(abs_path));
+
+//	get_path();
 
 	if (argc > 1)
-		path = argv[1];
+		dir_name = argv[1];
 	else
-		path = "./abc";
+		dir_name = "./abc";
+	if (argc > 2)
+		file_name = argv[2];
+	else
+		file_name = "c.c";
 
-	scan_dir(path, 0);
-	return 0;
+	if (argc == 3) {
+		r = search_dir(dir_name, file_name, abs_path);
+		if (!r)
+			printf("not find %s\n", file_name);
+		else
+			printf("file path: %s\n", abs_path);
+	} else {
+		r = scan_dir(dir_name, 0);
+		if (r)
+			printf("scan dir %s failed\n", dir_name);
+		else
+			printf("scan dir %s okay\n", dir_name);
+	}
+	return r;
 }
