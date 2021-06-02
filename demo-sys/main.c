b53ab60850bad8a341ae0c058b3fc4e1f32f79ad
daqingzhang <daqingzhang@bestechnic.com>
2021-06-02 15:22:18 +0800

    demo-sys: add timer usage

--- demo-sys/main.c
+++ demo-sys/main.c
@@ -2,6 +2,74 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/time.h>
+#include <signal.h>
+#include <time.h>
+#include <ncurses.h>
+
+void print_local_time(void)
+{
+	static time_t t1;
+	static struct tm *pt;
+	static int tick = 0;
+	int i = 0, n;
+
+	time(&t1);
+	pt = localtime(&t1);
+
+	n = 4+2+2+2+2+2+8;
+	if (tick > 0) {
+		for (i = 0; i < n; i++) {
+			printf("\b");
+		}
+	}
+	printf("[%04d,%02d,%02d][%02d:%02d:%02d]",
+		pt->tm_year+1900,pt->tm_mon, pt->tm_mday, pt->tm_hour, pt->tm_min, pt->tm_sec);
+
+	tick++;
+	fflush(stdout);
+}
+
+void timer_handler(int signum)
+{
+#if 0
+	static int tick = 0;
+		for (int i = 0; i < tick + (10-1) / 10; i++) {
+			printf("\b");
+		}
+		printf("%d",tick);
+		tick++;
+		fflush(stdout);
+#else
+	print_local_time();
+#endif
+}
+
+int test_timer(void)
+{
+#define MS_TO_US(ms) (ms*1000)
+	int r = 0;
+	struct itimerval timer;
+	struct sigaction timer_sa;
+
+	printf("%s:\n", __func__);
+
+	memset(&timer_sa, 0, sizeof(timer_sa));
+	timer_sa.sa_handler = timer_handler;
+	sigaction(SIGALRM, &timer_sa, NULL);
+
+	timer.it_interval.tv_sec  = 1;
+	timer.it_interval.tv_usec = MS_TO_US(100);
+	timer.it_value.tv_sec     = timer.it_interval.tv_sec;
+	timer.it_value.tv_usec    = timer.it_interval.tv_usec;
+
+	r = setitimer(ITIMER_REAL, &timer, NULL);
+	if (r) {
+		printf("%s: setitimer failed %d", __func__, r);
+	}
+	while(1);
+	return r;
+}
 
 int test_readlink(int argc, char *argv[])
 {
@@ -73,6 +141,7 @@ int test_access(int argc, char *argv[])
 int main(int argc, char *argv[])
 {
 //	test_readlink(argc, argv);
-	test_access(argc, argv);
+//	test_access(argc, argv);
+	test_timer();
 	return 0;
 }
